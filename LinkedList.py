3240a1dc69fc385cf28f147f7f98235ee28ac383
Grzegorz121979 <pociechagrzegorz@outlook.com>
2024-02-10 22:18:28 +0100

    insert at index function

--- LinkedList.py
+++ LinkedList.py
@@ -13,3 +13,20 @@ class LinkedList:
         else:
             new_node.next = self.head
             self.head = new_node
+
+    def insertAtIndex(self, data, index):
+        new_node = Node(data)
+        current_node = self.head
+        position = 0
+        if position == index:
+            self.insertAtBegin(data)
+        else:
+            while current_node is not None and position + 1 != index:
+                position = position + 1
+                current_node = current_node.next
+
+            if current_node is not None:
+                new_node.next = current_node.next
+                current_node.next = new_node
+            else:
+                print('Index not present')
